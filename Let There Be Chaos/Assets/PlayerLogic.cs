4de140e2cc3003f8e2c31bf08252a1d025232da2
frank06n <dev.code38@gmail.com>
2021-08-25 21:45:20 +0530

    protoype 2 + code refactor
    > Player Controls Revamp
    > Player Logic Code Refactor

--- Let There Be Chaos/Assets/PlayerLogic.cs
+++ Let There Be Chaos/Assets/PlayerLogic.cs
@@ -4,93 +4,113 @@ using UnityEngine;
 
 public class PlayerLogic : MonoBehaviour
 {
-    Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private float airtime;
+    private float lastJumpPress;
 
-    public Transform cam;
-    Vector3 cameraVelocity;
-    public Vector3 cameraOffset;
-    public float CameraFollowSmoothTime;
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private Vector3 cameraOffset;
+    [SerializeField] private float CameraFollowSmoothTime;
+    private Vector3 cameraVelocity;
 
-    public float SideForce;
-    public float MaxSideVelocity;
-    public float JumpImpulse;
+    [SerializeField] private float SideForce;
+    [SerializeField] private float MaxSideVelocity;
+    [SerializeField] private float JumpImpulse;
+    [SerializeField] private float JumpBuffer;
+    [SerializeField] private float CoyoteTime;
 
-    int bodyContacts;
+    private int bodyContacts;
 
-    // Start is called before the first frame update
+    
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         bodyContacts = 0;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
-        {
-            rb.velocity = new Vector2(rb.velocity.x, JumpImpulse);
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        Vector3.SmoothDamp(cam.position, transform.position + cameraOffset, ref cameraVelocity, CameraFollowSmoothTime);
-        cam.position = cam.position + cameraVelocity * Time.deltaTime;
-
-        //if (IsGrounded())
-        //    rb.velocity = new Vector2(MaxSideVelocity, rb.velocity.y);
-        //else
-        //    rb.velocity = new Vector2(MaxSideVelocity * 1f, rb.velocity.y);
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            //rb.AddForce(Vector2.left * SideForce * Time.deltaTime);
-            if (IsGrounded())
-                rb.velocity = new Vector2(-MaxSideVelocity, rb.velocity.y);
-            else
-                rb.velocity = new Vector2(-MaxSideVelocity * 1f, rb.velocity.y);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            //rb.AddForce(Vector2.right * SideForce * Time.deltaTime);
-            if (IsGrounded())
-                rb.velocity = new Vector2(MaxSideVelocity, rb.velocity.y);
-            else
-                rb.velocity = new Vector2(MaxSideVelocity * 1f, rb.velocity.y);
-        }
-        else
-        {
-            if (IsGrounded())
-                rb.velocity = new Vector2(0, rb.velocity.y);
-            else
-                rb.velocity = new Vector2(rb.velocity.x - rb.velocity.x * Time.deltaTime * 2f, rb.velocity.y);
-        }
-
-
-
-        //rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -MaxSideVelocity, MaxSideVelocity), rb.velocity.y);
-    }
-
-
     private bool IsGrounded()
     {
         return bodyContacts > 0;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void CheckJump()
     {
-        if (collision.collider.CompareTag("Platform"))
+        if (lastJumpPress <= JumpBuffer && airtime <= CoyoteTime)
+            rb.velocity = new Vector2(rb.velocity.x, JumpImpulse);
+    }
+
+    private void Update()
+    {
+        if (IsGrounded())   airtime = 0;
+        else                airtime += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))    lastJumpPress = 0;
+        else                                    lastJumpPress += Time.deltaTime;
+
+        CheckJump();
+    }
+
+    private void SmoothCameraFollow()
+    {
+        Vector3.SmoothDamp(cameraTransform.position, transform.position + cameraOffset, ref cameraVelocity, CameraFollowSmoothTime);
+        cameraTransform.position = cameraTransform.position + cameraVelocity * Time.deltaTime;
+    }
+
+    private void LookTowards(int direction)
+    {
+        float scaleX = direction * Mathf.Abs(transform.localScale.x);
+        transform.localScale = new Vector3(scaleX, transform.localScale.y, 1);
+    }
+
+    private void MoveTowards(int direction)
+    {
+        LookTowards(direction);
+        rb.velocity = new Vector2(direction * MaxSideVelocity, rb.velocity.y);
+    }
+
+    private void AddFrictionForce()
+    {
+        if (IsGrounded())
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        else
+            rb.velocity = new Vector2(rb.velocity.x - rb.velocity.x * Time.deltaTime * 2f, rb.velocity.y);
+    }
+
+    private void FixedUpdate()
+    {
+        SmoothCameraFollow();
+
+
+        const int SIDE_LEFT  = -1;
+        const int SIDE_RIGHT = +1;
+
+        if (Input.GetKey(KeyCode.A))
         {
-            bodyContacts++;
+            MoveTowards(SIDE_LEFT);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            MoveTowards(SIDE_RIGHT);
+        }
+        else {
+            AddFrictionForce();
         }
     }
+
+    private bool IsPlatform(Collision2D collision)
+    {
+        return collision.collider.CompareTag("Platform");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (IsPlatform(collision)) bodyContacts++;
+    }
+    
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.collider.CompareTag("Platform"))
-        {
-            bodyContacts--;
-        }
+        if (IsPlatform(collision)) bodyContacts--;
     }
 
 }
