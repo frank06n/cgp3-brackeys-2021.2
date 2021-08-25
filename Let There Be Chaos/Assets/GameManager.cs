d001cb9d0e744b291a9d9b114c6adcfed7c49e0e
frank06n <dev.code38@gmail.com>
2021-08-25 19:46:57 +0530

    prototype 1 + test sprite
    > Player Controls
    > Test player sprite

---
+++ Let There Be Chaos/Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        StartCoroutine(GameStart());
+
+    }
+
+    IEnumerator GameStart()
+    {
+        Time.timeScale = 0;
+        yield return new WaitForSecondsRealtime(2);
+        Debug.Log("start");
+        Time.timeScale = 1;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
