38db2b42024143429191b5074d2fd7c05387e00f
frank06n <dev.code38@gmail.com>
2021-11-23 08:38:01 +0530

    Minor graphics update for v02
    > changed ground prefabs
    > changed other prefabs
    > added Bait script for cleaning hierarchy

---
+++ Let There Be Chaos/Assets/BaitEditor.cs
@@ -0,0 +1,39 @@
+﻿//C# Example (LookAtPointEditor.cs)
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Bait))]
+public class BaitEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Newfy"))
+        {
+            Transform platform = ((Bait)target).transform;
+            Transform topedge = platform.GetChild(0);
+            Transform platform_newg = platform.GetChild(1);
+
+            SpriteRenderer org_sr = platform.GetComponent<SpriteRenderer>();
+            SpriteRenderer newg_sr = platform_newg.GetComponent<SpriteRenderer>();
+
+            BoxCollider2D bc = platform.GetComponent<BoxCollider2D>();
+
+            float factor = platform_newg.localScale.x;
+
+            platform.localScale = new Vector3(5 * factor, 5 * factor, 1);
+            topedge.localScale = new Vector3(topedge.localScale.x / factor, 1, 1);
+            topedge.localPosition = topedge.localPosition / factor;
+
+            org_sr.sprite = newg_sr.sprite;
+            bc.size = bc.size / factor;
+
+            org_sr.enabled = true;
+
+            DestroyImmediate(platform_newg.gameObject);
+            DestroyImmediate(target);
+        }
+    }
+}
--- Let There Be Chaos/Assets/Scripts/Gameplay Scripts/CollectibleLogic.cs
+++ Let There Be Chaos/Assets/Scripts/Gameplay Scripts/CollectibleLogic.cs
@@ -14,6 +14,11 @@ public class CollectibleLogic : MonoBehaviour
 
     [SerializeField] private CType CollectibleType;
     [SerializeField] private int Value;
+    [SerializeField] private float HoverYChange;
+    [SerializeField] private float HoverSpeed;
+
+    private float initLocalY;
+    private float hoverDelta;
 
 
     private void Start()
@@ -21,7 +26,19 @@ public class CollectibleLogic : MonoBehaviour
         if (CollectibleType==CType.MEMORY)
         {
             LevelManager.instance.memoryCount++;
-        }  
+        }
+        initLocalY = transform.localPosition.y;
+        hoverDelta = Random.Range(0f, 2 * Mathf.PI);
+    }
+
+    private void Update()
+    {
+        if (HoverYChange>0)
+        {
+            hoverDelta += HoverSpeed * Time.deltaTime;
+            float change = Mathf.Sin(hoverDelta) * HoverYChange;
+            transform.localPosition = new Vector3(transform.localPosition.x, initLocalY + change, 0);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
